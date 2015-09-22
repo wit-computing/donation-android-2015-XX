39ecb6b49bfdea9be3607adfc340dd3f4e0f6e6e
Eamonn de Leastar <edeleastar@wit.ie>
2015-09-22 12:39:02 +0100

    Donation 03: Exercise 10: Users validation introduced

--- app/src/main/java/app/donation/DonationApp.java
+++ app/src/main/java/app/donation/DonationApp.java
@@ -35,6 +35,18 @@ public class DonationApp extends Application
     users.add(user);
   }
 
+  public boolean validUser (String email, String password)
+  {
+    for (User user : users)
+    {
+      if (user.email.equals(email) && user.password.equals(password))
+      {
+        return true;
+      }
+    }
+    return false;
+  }
+
   @Override
   public void onCreate()
   {
--- app/src/main/java/app/donation/Login.java
+++ app/src/main/java/app/donation/Login.java
@@ -4,6 +4,9 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import app.donation.R;
 
 public class Login extends Activity
@@ -17,6 +20,19 @@ public class Login extends Activity
 
   public void signinPressed (View view)
   {
-    startActivity (new Intent(this, Donate.class));
+    DonationApp app = (DonationApp) getApplication();
+
+    TextView email     = (TextView)  findViewById(R.id.loginEmail);
+    TextView password  = (TextView)  findViewById(R.id.loginPassword);
+
+    if (app.validUser(email.getText().toString(), password.getText().toString()))
+    {
+      startActivity (new Intent(this, Donate.class));
+    }
+    else
+    {
+      Toast toast = Toast.makeText(this, "Invalid Credentials", Toast.LENGTH_SHORT);
+      toast.show();
+    }
   }
 }
