51ce5118be46e593e2abee0ff27b0d3057f18bbf
Eamonn de Leastar <edeleastar@wit.ie>
2015-11-08 13:26:21 +0000

    signup creates user via api

--- app/src/main/java/app/donation/activity/Signup.java
+++ app/src/main/java/app/donation/activity/Signup.java
@@ -5,19 +5,26 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
 
 import app.donation.R;
+import app.donation.api.CreateUser;
+import app.donation.api.Response;
 import app.donation.main.DonationApp;
 import app.donation.model.User;
 
-public class Signup extends AppCompatActivity
+public class Signup extends AppCompatActivity implements Response<User>
 {
+  private DonationApp app;
 
   @Override
   protected void onCreate(Bundle savedInstanceState)
   {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_signup);
+    app = (DonationApp) getApplication();
   }
 
   public void registerPressed (View view)
@@ -30,8 +37,29 @@ public class Signup extends AppCompatActivity
     User user = new User (firstName.getText().toString(), lastName.getText().toString(), email.getText().toString(), password.getText().toString());
 
     DonationApp app = (DonationApp) getApplication();
-    app.newUser(user);
 
+    new CreateUser(user, app.donationServiceAPI, this, this, "Creating new User Account").execute();
+
+    startActivity (new Intent(this, Welcome.class));
+  }
+
+  @Override
+  public void setResponse(List<User> aList)
+  {}
+
+  @Override
+  public void setResponse(User user)
+  {
+    app.users.add(user);
+    startActivity (new Intent(this, Welcome.class));
+  }
+
+  @Override
+  public void errorOccurred(Exception e)
+  {
+    app.donationServiceAvailable = false;
+    Toast toast = Toast.makeText(this, "Donation Service Unavailable. Try again later", Toast.LENGTH_LONG);
+    toast.show();
     startActivity (new Intent(this, Welcome.class));
   }
 }
