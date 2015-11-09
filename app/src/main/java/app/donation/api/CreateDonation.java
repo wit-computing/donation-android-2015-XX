430caaf4143a4e6df003c50218a89a915a410f4d
Eamonn de Leastar <edeleastar@wit.ie>
2015-11-09 07:50:01 +0000

    adjust commands to use refactored api

--- app/src/main/java/app/donation/api/CreateDonation.java
+++ app/src/main/java/app/donation/api/CreateDonation.java
@@ -4,6 +4,7 @@ import android.content.Context;
 
 import java.util.List;
 
+import app.donation.main.DonationApp;
 import app.donation.model.Donation;
 import app.donation.model.User;
 
@@ -11,18 +12,20 @@ public class CreateDonation extends Request
 {
   DonationServiceAPI api;
   Donation donation;
+  User user;
 
-  public CreateDonation(Donation donation, DonationServiceAPI api, Context context, Response<Donation> callback, String message)
+  public CreateDonation(User user, Donation donation, DonationServiceAPI api, Context context, Response<Donation> callback, String message)
   {
     super(context, callback, message);
     this.api = api;
+    this.user = user;
     this.donation = donation;
   }
 
   @Override
   protected Donation doRequest(Object... params) throws Exception
   {
-    Donation returnedDonation = api.createDonation(donation);
+    Donation returnedDonation = api.createDonation(user, donation);
     return returnedDonation;
   }
 }
--- app/src/main/java/app/donation/api/GetDonations.java
+++ app/src/main/java/app/donation/api/GetDonations.java
@@ -5,21 +5,24 @@ import android.content.Context;
 import java.util.List;
 
 import app.donation.model.Donation;
+import app.donation.model.User;
 
 public class GetDonations extends Request
 {
   DonationServiceAPI api;
+  User user;
 
-  public GetDonations(DonationServiceAPI api, Context context, Response<Donation> callback, String message)
+  public GetDonations(User user, DonationServiceAPI api, Context context, Response<Donation> callback, String message)
   {
     super(context, callback, message);
     this.api = api;
+    this.user = user;
   }
 
   @Override
   protected List<Donation> doRequest(Object... params) throws Exception
   {
-    List<Donation> donationList = api.getDonations();
+    List<Donation> donationList = api.getDonations(user);
     return donationList;
   }
 }
